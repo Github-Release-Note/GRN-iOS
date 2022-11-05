6a9ebb0f44bf2a7408249d182fdeea3d3db12c24
JiHoonAHN <ahnjh1028@naver.com>
2022-11-06 03:35:29 +0900

    Make App Name

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -1,8 +1,8 @@
 import ProjectDescription
 
 public enum Environment {
-    public static let appName = ""
-    public static let targetName = ""
+    public static let appName = "GRN-iOS"
+    public static let targetName = "GRN-iOS"
     public static let targetTestName = "\(targetName)Tests"
     public static let organizationName = "JiHoonAHN"
     public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "14.0", devices: [.iphone, .ipad])
