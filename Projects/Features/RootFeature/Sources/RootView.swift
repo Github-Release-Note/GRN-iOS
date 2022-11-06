bf867e7c177152a68a7506e084ff5e573efb3479
JiHoonAHN <ahnjh1028@naver.com>
2022-11-06 19:14:29 +0900

    Update Content

--- Projects/Features/RootFeature/Sources/RootView.swift
+++ Projects/Features/RootFeature/Sources/RootView.swift
@@ -8,3 +8,9 @@ public struct RootView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+
+struct MyPreviewProvider_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("Hello, world!")
+    }
+}
